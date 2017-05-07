a3175d190addace569b8bcbd3fe8ea5f5712c204
Gul Imran <gimran.it@gmail.com>
2017-05-07 18:22:27 +0100

    Refactor on click select to stateless functional component

---
+++ stateless-functional-components/app/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var App = require('./components/App');
+require('./index.css')
+
+ReactDom.render(
+  <App />,
+  document.getElementById('app')
+);
